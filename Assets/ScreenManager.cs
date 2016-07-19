117b9bf7952d9961bc00d7ef95e243cff4271213
marcjehu29 <marcjehu29@gmail.com>
2016-07-20 00:17:35 +0800

    message

---
+++ Assets/ScreenManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenManager : MonoBehaviour {
+
+    GameObject unita, unitb, unitc, unitd;
+
+    public void Start()
+    {
+        unita = GameObject.Find("unita");
+        unitb = GameObject.Find("unitb");
+        unitc = GameObject.Find("unitc");
+        unitd = GameObject.Find("unitd");
+    }
+
+    public void Exit()
+    {
+            Application.Quit();
+    }
+    public void Lessons()
+    {
+        unita.SetActive(true);
+        unitb.SetActive(true);
+        unitc.SetActive(true);
+        unitd.SetActive(true);
+        GameObject.Find("Lessons").SetActive(false);
+        GameObject.Find("Quiz").SetActive(false);
+        GameObject.Find("Trivia").SetActive(false);
+        GameObject.Find("Exit").SetActive(false);
+
+    }
+}
