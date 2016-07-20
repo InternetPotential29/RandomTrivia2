6ad8f47fee549242587c8f77e09965dc77a55f14
marcjehu29 <marcjehu29@gmail.com>
2016-07-20 10:24:26 +0800

    message

--- Assets/ScreenManager.cs
+++ Assets/ScreenManager.cs
@@ -3,30 +3,85 @@ using System.Collections;
 
 public class ScreenManager : MonoBehaviour {
 
-    GameObject unita, unitb, unitc, unitd;
+    public GameObject Unita, Unitb, Unitc, Unitd, U1C1, U1C1L1, U1C1L2, U1C1L3;
 
     public void Start()
     {
-        unita = GameObject.Find("unita");
-        unitb = GameObject.Find("unitb");
-        unitc = GameObject.Find("unitc");
-        unitd = GameObject.Find("unitd");
+        Unita = GameObject.Find("Unita");
+        Unitb = GameObject.Find("Unitb");
+        Unitc = GameObject.Find("Unitc");
+        Unitd = GameObject.Find("Unitd");
+        U1C1 = GameObject.Find("U1C1");
+        U1C1L1 = GameObject.Find("U1C1L1");
+        U1C1L2 = GameObject.Find("U1C1L2");
+        U1C1L3 = GameObject.Find("U1C1L3");
+        Unita.SetActive(false);
+        Unitb.SetActive(false);
+        Unitc.SetActive(false);
+        Unitd.SetActive(false);
+        U1C1.SetActive(false);
+        U1C1L1.SetActive(false);
+        U1C1L2.SetActive(false);
+        U1C1L3.SetActive(false);
     }
 
-    public void Exit()
+    public void CLICK(string name)
     {
+
+        if (name == "Exit")
+        {
             Application.Quit();
+        }
+        if(name == "Return")
+        {
+            GameObject.Find("Lessons").SetActive(true);
+            GameObject.Find("Quiz").SetActive(true);
+            GameObject.Find("Trivia").SetActive(true);
+            GameObject.Find("Exit").SetActive(true);
+            Unita.SetActive(false);
+            Unitb.SetActive(false);
+            Unitc.SetActive(false);
+            Unitd.SetActive(false);
+        }
+        if(name == "Lessons")
+        {
+            
+            GameObject.Find("Lessons").SetActive(false);
+            GameObject.Find("Quiz").SetActive(false);
+            GameObject.Find("Trivia").SetActive(false);
+            GameObject.Find("Exit").SetActive(false);
+            Unita.SetActive(true);
+            Unitb.SetActive(true);
+            Unitc.SetActive(true);
+            Unitd.SetActive(true);
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                CLICK("Return");
+            }
+        }
+        if(name == "U1")
+        {
+            Unita.SetActive(false);
+            Unitb.SetActive(false);
+            Unitc.SetActive(false);
+            Unitd.SetActive(false);
+            U1C1.SetActive(true);
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                CLICK("Lessons");
+            }
+        }
+        if(name == "U1C1")
+        {
+            U1C1.SetActive(false);
+            U1C1L1.SetActive(true);
+            U1C1L2.SetActive(true);
+            U1C1L3.SetActive(true);
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                CLICK("U1");
+            }
+        }
     }
-    public void Lessons()
-    {
-        unita.SetActive(true);
-        unitb.SetActive(true);
-        unitc.SetActive(true);
-        unitd.SetActive(true);
-        GameObject.Find("Lessons").SetActive(false);
-        GameObject.Find("Quiz").SetActive(false);
-        GameObject.Find("Trivia").SetActive(false);
-        GameObject.Find("Exit").SetActive(false);
 
-    }
 }
