b5af939057253694b4385252a1fcaeac809e1b71
marcjehu29 <marcjehu29@gmail.com>
2016-07-20 15:44:25 +0800

    message

--- Assets/ScreenManager.cs
+++ Assets/ScreenManager.cs
@@ -3,18 +3,41 @@ using System.Collections;
 
 public class ScreenManager : MonoBehaviour {
 
-    public GameObject Unita, Unitb, Unitc, Unitd, U1C1, U1C1L1, U1C1L2, U1C1L3;
-
+    public GameObject Lessons, Quiz, Trivia, Exit, Back, 
+        QU1, QU2, QU3, QU4, 
+        Unita, Unitb, Unitc, 
+        Unitd, 
+        U1C1, U1C1L1, U1C1L2, U1C1L3;
+    public string chosen;
     public void Start()
     {
+        Lessons = GameObject.Find("Lessons");
+        Quiz = GameObject.Find("Quiz");
+        Trivia = GameObject.Find("Trivia");
+        Exit = GameObject.Find("Exit");
+        Back = GameObject.Find("Back");
+
+        QU1 = GameObject.Find("QU1");
+        QU2 = GameObject.Find("QU2");
+        QU3 = GameObject.Find("QU3");
+        QU4 = GameObject.Find("QU4");
+
         Unita = GameObject.Find("Unita");
         Unitb = GameObject.Find("Unitb");
         Unitc = GameObject.Find("Unitc");
         Unitd = GameObject.Find("Unitd");
+
         U1C1 = GameObject.Find("U1C1");
+
         U1C1L1 = GameObject.Find("U1C1L1");
         U1C1L2 = GameObject.Find("U1C1L2");
         U1C1L3 = GameObject.Find("U1C1L3");
+
+        Back.SetActive(false);
+        QU1.SetActive(false);
+        QU2.SetActive(false);
+        QU3.SetActive(false);
+        QU4.SetActive(false);
         Unita.SetActive(false);
         Unitb.SetActive(false);
         Unitc.SetActive(false);
@@ -27,37 +50,35 @@ public class ScreenManager : MonoBehaviour {
 
     public void CLICK(string name)
     {
-
         if (name == "Exit")
         {
             Application.Quit();
         }
-        if(name == "Return")
-        {
-            GameObject.Find("Lessons").SetActive(true);
-            GameObject.Find("Quiz").SetActive(true);
-            GameObject.Find("Trivia").SetActive(true);
-            GameObject.Find("Exit").SetActive(true);
-            Unita.SetActive(false);
-            Unitb.SetActive(false);
-            Unitc.SetActive(false);
-            Unitd.SetActive(false);
-        }
+
         if(name == "Lessons")
         {
             
-            GameObject.Find("Lessons").SetActive(false);
-            GameObject.Find("Quiz").SetActive(false);
-            GameObject.Find("Trivia").SetActive(false);
-            GameObject.Find("Exit").SetActive(false);
+            Lessons.SetActive(false);
+            Quiz.SetActive(false);
+            Trivia.SetActive(false);
+            Exit.SetActive(false);
+            Back.SetActive(true);
             Unita.SetActive(true);
             Unitb.SetActive(true);
             Unitc.SetActive(true);
             Unitd.SetActive(true);
-            if (Input.GetKeyDown(KeyCode.Escape))
-            {
-                CLICK("Return");
-            }
+        }
+        if(name == "Quiz")
+        {
+            Lessons.SetActive(false);
+            Quiz.SetActive(false);
+            Trivia.SetActive(false);
+            Exit.SetActive(false);
+            Back.SetActive(true);
+            QU1.SetActive(true);
+            QU2.SetActive(true);
+            QU3.SetActive(true);
+            QU4.SetActive(true);
         }
         if(name == "U1")
         {
@@ -66,10 +87,6 @@ public class ScreenManager : MonoBehaviour {
             Unitc.SetActive(false);
             Unitd.SetActive(false);
             U1C1.SetActive(true);
-            if (Input.GetKeyDown(KeyCode.Escape))
-            {
-                CLICK("Lessons");
-            }
         }
         if(name == "U1C1")
         {
@@ -77,11 +94,47 @@ public class ScreenManager : MonoBehaviour {
             U1C1L1.SetActive(true);
             U1C1L2.SetActive(true);
             U1C1L3.SetActive(true);
-            if (Input.GetKeyDown(KeyCode.Escape))
-            {
-                CLICK("U1");
-            }
         }
     }
+    public void BackPress()
+    {
+        if (QU1.activeSelf == true)
+        {
+            Lessons.SetActive(true);
+            Quiz.SetActive(true);
+            Trivia.SetActive(true);
+            Exit.SetActive(true);
+            Back.SetActive(false);
+            QU1.SetActive(false);
+            QU2.SetActive(false);
+            QU3.SetActive(false);
+            QU4.SetActive(false);
+        }
+        else if(U1C1L1.activeSelf == true)
+        {
+            U1C1.SetActive(true);
+            U1C1L1.SetActive(false);
+            U1C1L2.SetActive(false);
+            U1C1L3.SetActive(false);
+        }else if(U1C1.activeSelf == true)
+        {
+            Unita.SetActive(true);
+            Unitb.SetActive(true);
+            Unitc.SetActive(true);
+            Unitd.SetActive(true);
+            U1C1.SetActive(false);
+        }else if(Unita.activeSelf == true)
+        {
 
+            Lessons.SetActive(true);
+            Quiz.SetActive(true);
+            Trivia.SetActive(true);
+            Exit.SetActive(true);
+            Back.SetActive(false);
+            Unita.SetActive(false);
+            Unitb.SetActive(false);
+            Unitc.SetActive(false);
+            Unitd.SetActive(false);
+        }
+    }
 }
